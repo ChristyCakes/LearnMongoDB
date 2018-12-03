737f5c8be34cff0423cd71b23acf36abdba07b8c
Christy Welsh <christyannwelsh@gmail.com>
2018-12-03 10:00:00 -0800

    add queries

--- routes/api.js
+++ routes/api.js
@@ -1,44 +1,66 @@
 // Full Documentation - https://www.turbo360.co/docs
-const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
-const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const turbo = require('turbo360')({ site_id: process.env.TURBO_APP_ID })
+const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID })
 const router = vertex.router()
 
 const Profile = require('../models/profile')
 const Team = require('../models/team')
 
-// get handler fetches all profiles in db
 router.get('/profile', (req, res) => {
 
-	Profile.find()
-	.then(profiles => {
-		res.json({
-			confirmation: 'success',
-			data: profiles
+	let filters = req.query
+	if (req.query.age != null) {
+		filters = {
+			age: { $gt: req.query.age }
+		}
+	}
+	Profile.find(filters)
+		.then(profiles => {
+			res.json({
+				confirmation: 'success',
+				data: profiles
+			})
 		})
-	})
-	.catch(err => {
-		res.json({
-			confimation: 'fail',
-			message: err.message
+		.catch(err => {
+			res.json({
+				confimation: 'fail',
+				message: err.message
+			})
 		})
-	})
 })
 
 router.get('/team', (req, res) => {
 
 	Team.find()
-	.then(teams => {
-		res.json({
-			confirmation: 'success',
-			data: teams
+		.then(teams => {
+			res.json({
+				confirmation: 'success',
+				data: teams
+			})
 		})
-	})
-	.catch(err => {
-		res.json({
-			confimation: 'fail',
-			message: err.message
+		.catch(err => {
+			res.json({
+				confimation: 'fail',
+				message: err.message
+			})
 		})
-	})
+})
+
+router.get('/profile/:id', (req, res) => {
+	const id = req.params.id
+
+	Profile.findById(id)
+		.then(profile => {
+			res.json({
+				confirmation: 'succes',
+				data: profile
+			})
+		}).catch(err => {
+			res.json({
+				confirmation: 'fail',
+				message: `Profile ${id} not found. ${err.message}`
+			})
+		})
 })
 
 
